3f60fbcd23738c58d851ad573062f43bbf629c8d
Steven Jackson <stevenpjackson0@gmail.com>
2019-11-04 15:13:27 -0600

    Add: 2 return function

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
 /* !Function Returns! */
 
+/*
 //Create function with return
 function hello(){
     return "Hello, World.";
@@ -11,4 +12,21 @@ $greeting = hello();
 
 //Echo returned string within variable 
 echo $greeting; 
+*/
+
+//Create function with 2 returns 
+function hello($name){
+    if($name == "Mike"){
+    return "Hello, Mike.";
+    }else{
+        return "Hello, stranger.";
+    }
+}
+
+//Pass function argument and assign return to $greeting
+$greeting = hello('Mike');
+
+//Echo returned value
+echo $greeting;
+
 ?>
