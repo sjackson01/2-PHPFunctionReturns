b0773a2a51ea3eaec0bd9047d34837e413cfbfbb
Steven Jackson <stevenpjackson0@gmail.com>
2019-11-04 14:48:10 -0600

    Add: Funciton with return value

--- index.php
+++ index.php
@@ -1,3 +1,12 @@
 <?php
+//Create function with return
+function hello(){
+    return "Hello, World.";
+}
 
+//Assign returned value to a variable
+$greeting = hello();
+
+//Echo returned string within variable 
+echo $greeting; 
 ?>
