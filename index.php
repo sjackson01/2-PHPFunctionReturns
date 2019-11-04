36d21fcadf7497b313dd73ab40abdcaa9ae13531
Steven Jackson <stevenpjackson0@gmail.com>
2019-11-04 15:26:53 -0600

    Add: Function that returns array

--- index.php
+++ index.php
@@ -14,6 +14,7 @@ $greeting = hello();
 echo $greeting; 
 */
 
+/*
 //Create function with 2 returns 
 function hello($name){
     if($name == "Mike"){
@@ -28,5 +29,28 @@ $greeting = hello('Mike');
 
 //Echo returned value
 echo $greeting;
+*/
+
+//Create function that returns array
+function add_up($a, $b){
+    //Create internal array
+    $arr = array(
+        $a,
+        $b,
+        $a + $b,
+    );
+    return $arr;
+
+}
+
+//Pass arguments to function
+$value = add_up(2,4);
+
+//Array ( [0] => 2 [1] => 4 [2] => 6 )
+//Print out array
+print_r($value); 
+
+//Echo single array value
+echo $value[0];
 
 ?>
